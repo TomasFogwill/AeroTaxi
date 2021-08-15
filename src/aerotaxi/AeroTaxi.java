db161d575786719c41e2ddaa4f701640da5a5605
Tomas Fogwill <tomasfogwill@gmail.com>
2021-08-15 12:23:25 -0300

    First commit

--- src/aerotaxi/AeroTaxi.java
+++ src/aerotaxi/AeroTaxi.java
@@ -15,7 +15,7 @@ public class AeroTaxi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Comenzo el proyecto, parte 1000");
     }
     
 }
