e08b6c42169ebcd733bb4561e8de535cc6e83db5
Tomas Fogwill <tomasfogwill@gmail.com>
2021-08-17 17:22:36 -0300

    Parece que por ahora va andando, cree las clases principales

--- src/aerotaxi/AeroTaxi.java
+++ src/aerotaxi/AeroTaxi.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aerotaxi;
 
-/**
- *
- * @author Tomas
- */
+
 public class AeroTaxi {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         System.out.println("Comenzo el proyecto, parte 1000, la concha de tu hermana");
     }
