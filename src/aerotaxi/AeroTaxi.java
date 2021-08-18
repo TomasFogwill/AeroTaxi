a6a4d5501af92f1cbd16414bf20de6856406a1b2
Tomas Fogwill <tomasfogwill@gmail.com>
2021-08-18 18:42:44 -0300

    Encaminando 2

--- src/aerotaxi/AeroTaxi.java
+++ src/aerotaxi/AeroTaxi.java
@@ -1,12 +1,26 @@
 
 package aerotaxi;
 
+import java.time.LocalDate;
 
-public class AeroTaxi {
 
+public abstract class AeroTaxi {
+
+    public static void recorrido(){
+    
+    }
    
+  public static void contratar(){
+      LocalDate.of(2010, 2, 15);
+
+}  
+    
+    
     public static void main(String[] args) {
-        System.out.println("Comenzo el proyecto, parte 1000, la concha de tu hermana");
+       Recorrido r=new Recorrido();
+    
+    
+    
     }
     
 }
